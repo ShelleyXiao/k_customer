a4e81f97ab9f7a550c955367ba35b95eb2586f32
ShelleyXiao <xiao244164200@qq.com>
2017-09-25 21:03:33 +0800

    列表基础组件

---
+++ app/src/main/java/com/kidoo/customer/media/ImageFolderPopupWindow.java
@@ -0,0 +1,20 @@
+package com.kidoo.customer.media;
+
+import android.view.View;
+import android.widget.PopupWindow;
+
+/**
+ * User: ShaudXiao
+ * Date: 2017-09-25
+ * Time: 20:39
+ * Company: zx
+ * Description:
+ * FIXME
+ */
+
+
+public class ImageFolderPopupWindow  extends PopupWindow implements View.OnAttachStateChangeListener {
+
+
+
+}
--- app/src/main/java/com/kidoo/customer/media/SelectFragment.java
+++ app/src/main/java/com/kidoo/customer/media/SelectFragment.java
@@ -1,7 +1,16 @@
 package com.kidoo.customer.media;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.kidoo.customer.R;
 import com.kidoo.customer.media.config.SelectOption;
 import com.kidoo.customer.ui.base.fragment.BaseFragment;
+import com.kidoo.customer.widget.EmptyLayout;
+
+import butterknife.Bind;
 
 /**
  * User: ShaudXiao
@@ -14,6 +23,22 @@ import com.kidoo.customer.ui.base.fragment.BaseFragment;
 
 
 public class SelectFragment extends BaseFragment {
+
+    @Bind(R.id.rv_image)
+    RecyclerView mContentView;
+    @Bind(R.id.btn_title_select)
+    Button mSelectFolderView;
+    @Bind(R.id.iv_title_select)
+    ImageView mSelectFolderIcon;
+    @Bind(R.id.toolbar)
+    View mToolbar;
+    @Bind(R.id.btn_done)
+    Button mDoneView;
+    @Bind(R.id.btn_preview)
+    Button mPreviewView;
+
+    @Bind(R.id.error_layout)
+    EmptyLayout mErrorLayout;
 
     private static SelectOption mOption;
 
