821f38b75e897fa6fe7e04a661a06fdfeea86299
ShelleyXiao <xiao244164200@qq.com>
2017-09-30 18:09:54 +0800

    mvp构架

---
+++ app/src/main/java/com/kidoo/customer/cipher/rsa/RSA.java
@@ -0,0 +1,19 @@
+package com.kidoo.customer.cipher.rsa;
+
+/**
+ * User: ShaudXiao
+ * Date: 2017-09-30
+ * Time: 17:03
+ * Company: zx
+ * Description:
+ * FIXME
+ */
+
+
+public class RSA {
+
+    public static final String RSA_PUBLICKEY = "";
+
+    public static final String RSA_PRIVATEKEY = "";
+
+}
---
+++ app/src/main/java/com/kidoo/customer/mvp/view/BaseView.java
@@ -0,0 +1,14 @@
+package com.kidoo.customer.mvp.view;
+
+/**
+ * User: ShaudXiao
+ * Date: 2017-09-30
+ * Time: 15:44
+ * Company: zx
+ * Description:
+ * FIXME
+ */
+
+
+public interface BaseView {
+}
