c5917142d6563367fbb613516cc20260dd2f08d5
Exyl <xiao244164200@qq.com>
2017-10-22 22:01:37 +0800

    #
    *

---
+++ app/src/main/java/com/kidoo/customer/adapter/broadcastDetail/model/IBroadcastDetailModel.java
@@ -0,0 +1,12 @@
+package com.kidoo.customer.adapter.broadcastDetail.model;
+
+/**
+ * description:
+ * autour: ShaudXiao
+ * date: 2017/10/22
+ * update: 2017/10/22
+ * version:
+*/
+
+public interface IBroadcastDetailModel {
+}
